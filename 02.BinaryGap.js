9da08875b921b060a37ea843e69bed3c9cdde660
david-commit <david.ondiege@students.moringaschool.com>
2023-01-24 13:15:06 +0300

    Added a html file

--- 02.BinaryGap.js
+++ 02.BinaryGap.js
@@ -1,9 +1,9 @@
 function binaryGap(number) {
  const binaryArr = number.toString(2).split('')
  console.log(binaryArr)
- 
  const zeroArr = []
  console.log(zeroArr);
+ 
  for (let i in binaryArr) {
   
   if (i == 0) {
