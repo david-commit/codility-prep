862503984e5b9d98f7de047278b90b126c7be663
david-commit <david.ondiege@students.moringaschool.com>
2023-01-24 17:10:10 +0300

    Integer Pallindrome exercise

--- 01.Pallindrome.js
+++ 01.Pallindrome.js
@@ -1,3 +1,31 @@
+// ==== PALLINDROME
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+const isPalindrome = function (x) {
+  const rX = x.toString().split('').reverse().join('');
+  if (x == rX) {
+    console.log(true)
+  } else {
+    console.log(false);
+  }
+};
+isPalindrome(121)
+isPalindrome(-121);
+
 function pallindrome(string) {
   const word = string;
   const rword = string.split('').reverse().join('');
